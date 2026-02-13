1129c6cf2e2a0395458c518585346136354de608
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-02-13 13:07:13 -0500

    conversion

--- benchmark/dynamic_int/ctint_spin_spin.py
+++ benchmark/dynamic_int/ctint_spin_spin.py
@@ -52,8 +52,9 @@ for bl, g_bl in S.G0_iw:
     print(S.G0_iw[bl])
     g0_tau = make_gf_from_fourier(g0)
     print(g0_tau[0,0])
-    go_tau_dlr = fit_gf_dlr(g0_tau, wmax=10.0, eps = 1e-10, symmetrize = True)
-    g_bl.data[:,0,0] = go_tau_dlr.data[:,0,0]
+    go_tau_dlr = fit_gf_dlr(g0_tau[0,0])#, wmax=10.0, eps = 1e-10, symmetrize = True)
+    go_iw = make_gf_dlr_imfreq(go_tau_dlr)
+    g_bl.data[:,0,0] = go_iw.data[:,0,0]
 
 Q_tau = GfImTime(indices=[0], statistic='Boson', beta=beta, n_points=n_tau_bosonic)
 Q_tau.data[:,0,0] = q_tau.data[:,0,0]
