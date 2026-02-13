48c95311cce93379287566b3d6f6d2b973189a18
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-02-13 13:01:06 -0500

    conversion

--- benchmark/dynamic_int/ctint_spin_spin.py
+++ benchmark/dynamic_int/ctint_spin_spin.py
@@ -53,7 +53,7 @@ for bl, g_bl in S.G0_iw:
     print(S.G0_iw[bl])
     g0_tau = make_gf_from_fourier(g0)
     print(g0_tau[0,0])
-    go_tau_dlr = fit_gf_dlr(g0_tau[0,0], wmax=10.0, eps = 1e-10, symmetrize = True)
+    go_tau_dlr = fit_gf_dlr(g0_tau[0,0], wmax=10.0, eps = 1e-10)#, symmetrize = True)
     g_bl.data[:,0,0] = go_tau_dlr.data[:,0,0]
 # for bl, g_bl in S.G0_iw:
 #     g_bl << g0
