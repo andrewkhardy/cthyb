4e5d07dacc90e71ad1b6831a905da9f25c217a0b
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-02-17 11:38:57 -0500

    another measurement

--- benchmark/dynamic_int/run_benchmark_cthyb.sh
+++ benchmark/dynamic_int/run_benchmark_cthyb.sh
@@ -23,6 +23,6 @@ source /mnt/home/ahardy/ccq-software-build/triqs/3_development/installation/shar
 #../CTHYB/benchmark/
 #mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 0.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
 #mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
-mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 1000000 --measure_O_tau 10
-mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 1000000 --measure_O_tau 50
-mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 1000000 --measure_O_tau 200 
+mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 10000000 --measure_O_tau 10
+mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 10000000 --measure_O_tau 50
+mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 10000000 --measure_O_tau 200 
