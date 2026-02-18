50151e9a92ebc5e8a2fe6be3edc89cf3b738a0a6
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-02-17 23:02:23 -0500

    what's wrong

---
+++ benchmark/dynamic_int/ctint_old_spin_spin.py
@@ -0,0 +1,125 @@
+# Single orbital with dynamical spin-spin interactions (CT-INT solver).
+# Adapted from ctseg_spin_spin.py for benchmarking against CT-SEG and CT-HYB.
+from triqs_ctint import Solver
+
+import argparse
+import triqs.utility.mpi as mpi
+from triqs.gf import *
+from triqs.gf.descriptors import Function
+from triqs.gf.tools import *
+from triqs.operators import n
+from triqs.gf.block_gf import *
+import h5
+from triqs.utility.h5diff import h5diff
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Run spin-spin benchmarking (CT-INT).')
+parser.add_argument('--U', type=float, default=4.0, help='U parameter')
+parser.add_argument('--J', type=float, default=1.0, help='J parameter')
+parser.add_argument('--i1', type=float, default=1.0,  help='i1 switch (0 or 1)')
+parser.add_argument('--i2', type=float, default=1.0,  help='i2 switch (0 or 1)')
+parser.add_argument('--i3', type=float, default=1.0,  help='i3 switch (0 or 1)')
+parser.add_argument('--i4', type=float, default=1.0,  help='i4 switch (0 or 1)')
+parser.add_argument('--i5', type=float, default=1.0,  help='i5 switch (0 or 1)')
+parser.add_argument('--beta', type=float, default=10.0, help='Inverse temperature')
+args, unknown = parser.parse_known_args()
+
+# Numerical values
+beta = args.beta
+U = args.U
+mu = U/2
+J = args.J
+i_1, i_2, i_3, i_4, i_5 = args.i1, args.i2, args.i3, args.i4, args.i5
+n_tau = 4096
+n_tau_bosonic = 2001
+
+# Solver construction parameters
+gf_struct = [('down', 1), ('up', 1)]
+S = Solver(beta = beta,
+           gf_struct = gf_struct,
+           n_tau = n_tau,
+           use_Jperp = True,
+            use_D = True,
+           dlr_wmax = 10.0
+)
+# Get inputs from reference file
+with h5.HDFArchive("ctint.ref.h5", 'r') as Af:
+    g0 = Af["dmft_loop/i_001/S/G0_iw/up"]
+    q_tau = Af["dmft_loop/i_000/Q_tau"]
+
+
+# Initialize G0_iw (CT-INT uses G0_iw, not Delta_tau)
+# Both spin channels get the same G0 (paramagnetic solution)
+g0_tau = make_gf_from_fourier(g0)
+g0_tau_dlr = fit_gf_dlr(g0_tau, w_max=10.0, eps = 1e-10, symmetrize = True)
+g0_iw = make_gf_dlr_imfreq(g0_tau_dlr)
+
+for bl, g_bl in S.G0_iw:
+    g_bl.data[:,0,0] = g0.data[:,0,0]
+
+Q_tau = GfImTime(target_shape=[1,1], statistic='Boson', beta=beta, n_points=n_tau_bosonic)
+Q_tau.data[:,0,0] = q_tau.data[:,0,0]
+
+# Convert Q_tau to DLR grid for frequency-domain input
+# Q_tau_dlr = fit_gf_dlr(Q_tau, w_max=10.0, eps=1e-10, symmetrize=True)
+# Q_iw_dlr = make_gf_dlr_imfreq(Q_tau_dlr)
+Q_iw = make_gf_from_fourier(Q_tau)
+
+# --------- Spin-spin interaction via Matsubara frequency (DLR) ---------
+S.Jperp_iw.data[:] = -(J) * Q_iw.data[:] * i_1
+S.D0_iw["up", "up"].data[:] = -0.25*J*Q_iw.data[:] * i_2
+S.D0_iw["down", "down"].data[:] = -0.25*J*Q_iw.data[:] * i_3
+S.D0_iw["up", "down"].data[:] = 0.25*J*Q_iw.data[:] * i_4
+S.D0_iw["down", "up"].data[:] = 0.25*J*Q_iw.data[:] * i_5
+
+# # --------- Alternative: Spin-spin interaction via tau interface ---------
+# S.Jperp_tau << -(J) * Q_tau * i_1
+# S.D0_tau["up", "up"] << -0.25*J*Q_tau * i_2
+# S.D0_tau["down", "down"] << -0.25*J*Q_tau * i_3
+# S.D0_tau["up", "down"] << 0.25*J*Q_tau * i_4
+# S.D0_tau["down", "up"] << 0.25*J*Q_tau * i_5
+block_names = ['up','dn']
+gf_struct = [(bl, 1) for bl in block_names]
+h_int = U * n(block_names[0],0)*n(block_names[1],0)
+
+S.solve(h_int=h_int,
+        n_cycles = 1000000,
+        length_cycle = 100,
+        n_warmup_cycles = 100,
+        random_seed = 34788,
+        measure_histogram = True,
+        measure_density = True,
+        measure_M4_iw = True,
+        n_iw_M4 = 5,
+        nfft_buf_size = 50,
+        measure_M3pp_tau = True,
+        measure_M3ph_tau = True,
+        measure_M3xph_tau = True,
+        n_iw_M3 = 10,
+        n_iW_M3 = 10,
+        n_tau_M3 = 41,
+        measure_chi2pp_tau = True,
+        measure_chi2ph_tau = True,
+        n_iw_chi2 = 10,
+        n_tau_chi2 = 21,
+        measure_chiAB_tau = True,
+        chi_A_vec = [n('up',0) + n('dn', 0)],
+        chi_B_vec = [n('up',0) + n('dn', 0)],
+        post_process = True )
+# -------- Save in Aive ---------
+if mpi.is_master_node():
+    filename = f"/mnt/home/ahardy/ceph/CTHYB_Data/spin_spin_ctint_old_J-{J}-U-{U}_{i_1}_{i_2}_{i_3}_{i_4}_{i_5}_b-{beta}.h5"
+    with h5.HDFArchive(filename, "w") as A:
+        A["G0_iw"] = S.G0_iw
+        A["G_iw"] = S.G_iw
+        A["G2_iw"] = S.G2_iw
+        A["chi3pp_iw"] = S.chi3pp_iw
+        A["chi3ph_iw"] = S.chi3ph_iw
+        A["chi3xph_iw"] = S.chi3xph_iw
+        A["chi2pp_iw"] = S.chi2pp_iw
+        A["chi2ph_iw"] = S.chi2ph_iw
+        A["chiAB_iw"] = S.chiAB_iw
+        A["chi2pp_tau_from_M3"] = S.chi2pp_tau_from_M3
+        A["chi2ph_tau_from_M3"] = S.chi2ph_tau_from_M3
+        A["chi2xph_tau_from_M3"] = S.chi2xph_tau_from_M3
+
