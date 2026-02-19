93c552a03aeccf3e9654ffcce7395b3d42afa44a
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-02-19 09:49:01 -0500

    OK

--- benchmark/dynamic_int/run_benchmark_measurements.sh
+++ benchmark/dynamic_int/run_benchmark_measurements.sh
@@ -19,9 +19,5 @@ source /mnt/home/ahardy/ccq-software-build/triqs/.triqs_dev/bin/activate
 source /mnt/home/ahardy/ccq-software-build/triqs/3_development/installation/share/triqs/triqsvars.sh
 ##################### run your code here #####################
 #../CTHYB/benchmark/
-mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 0.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
-mpirun python ctseg_spin_spin.py --J 1.0 --U 4.0 --i1 0.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
-mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
-mpirun python ctseg_spin_spin.py --J 1.0 --U 4.0 --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
-mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
-mpirun python ctseg_spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
+mpirun python spin_spin.py --J 0.25 --U 4.0 --i1 1.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0 --n_cycles 10000000 --measure_O_tau 50
+mpirun python ctseg_spin_spin.py --J 0.25 --U 4.0 --i1 1.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
