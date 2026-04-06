eb0aca361519e7561b37bb9985d55e65334445b8
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-04-06 09:55:17 -0400

    trying

--- benchmark/dynamic_int/run_compare.sh
+++ benchmark/dynamic_int/run_compare.sh
@@ -11,13 +11,11 @@
 #SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=1
 #SBATCH --time=72:00:00
-module --force purge
-MODULES="modules/2.4-20250724 fftw nfft openmpi/5.0.6 hdf5/mpi-1.12.3 boost/1.87.0  python/3.12.9 python-mpi/3.12.9 flexiblas/3.4.2 llvm/19.1.7"
-module load ${MODULES}
+
+module purge
 #source /mnt/home/ahardy/ccq-software-build/triqs/3_unst_nix2.3_llvm/installation/share/triqs/triqsvars.sh
 #source /mnt/home/ahardy/ccq-software-build/triqs/.triqs_dev/bin/activate
-source /mnt/home/ahardy/triqs_venv/bin/activate
-source /mnt/home/ahardy/ccq-software-build/triqs/unstable/installation/share/triqs/triqsvars.sh
+source /mnt/home/ahardy/ccq-software-build/triqs/unstable/installation/share/triqs/triqsenv.sh
 #source /mnt/home/ahardy/ccq-software-build/triqs/3_nu_nu/installation/share/triqs/triqsvars.sh
 
 ##################### run your code here #####################
