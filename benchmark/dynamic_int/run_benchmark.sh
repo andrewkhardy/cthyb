00f3890dc749e0d93186dc316a241773ce412636
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-04-15 09:30:52 -0400

    benchmarking

--- benchmark/dynamic_int/run_benchmark.sh
+++ benchmark/dynamic_int/run_benchmark.sh
@@ -18,6 +18,6 @@ module load ${MODULES}
 module load triqs/unstable
 ##################### run your code here #####################
 #../CTHYB/benchmark/
-mpirun python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0 --n_cycles 10000000 --measure_O_tau 250
-#mpirun python ctseg_spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
-#mpirun python ctint_spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
+mpirun -n 120 python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 10000000 --measure_O_tau 250
+#mpirun -n 120 python ctseg_spin_spin.py --J 1.0 --U 4.0  --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
+#mpirun -n 120 python ctint_spin_spin.py --J 1.0 --U 4.0  --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
