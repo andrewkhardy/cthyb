18dcb9c5b7b0b2d5d1b61aada1d368a852f39637
Andrew Hardy <ahardy@rusty1.cm.cluster>
2026-05-06 14:24:23 -0400

    learning

--- benchmark/dynamic_int/run_benchmark.sh
+++ benchmark/dynamic_int/run_benchmark.sh
@@ -11,13 +11,14 @@
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=1
 #SBATCH --time=72:00:00
-MODULES="modules/2.4 gcc flexiblas openmpi cmake ccache gmp fftw nfft hdf5/mpi boost python/3.12 python-mpi/3.12 intel-oneapi-mkl llvm/19 eigen mpfr"
+MODULES="devenv9/clang-py3-mkl llvm/20"
 module purge
+module use /mnt/home/wentzell/opt/modules
 module load ${MODULES}
-#module load triqs/3_unst_nix2.4_llvm
 module load triqs/unstable
 ##################### run your code here #####################
 #../CTHYB/benchmark/
 mpirun -n 120 python spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0 --n_cycles 100000 --measure_O_tau 50
+#mpirun -n 120 python spin_spin.py --J 1.0 --U 4.0 --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0 --n_cycles 100000 --measure_O_tau 50
 #mpirun -n 120 python ctseg_spin_spin.py --J 1.0 --U 4.0  --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
 #mpirun -n 120 python ctint_spin_spin.py --J 1.0 --U 4.0  --i1 1.0 --i2 0.0 --i3 0.0 --i4 0.0  --i5 0.0 --beta 10.0
