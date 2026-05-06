d34c47fc657646512fcf5775f525246cfdaaaee3
Andrew Hardy <ahardy@worker6015.cm.cluster>
2026-05-06 14:30:55 -0400

    learning

--- benchmark/dynamic_int/spin_spin.py
+++ benchmark/dynamic_int/spin_spin.py
@@ -110,7 +110,7 @@ if mpi.is_master_node():
         A["average_sign"] = S.average_sign
         A["O_tau"] = S.O_tau#[(Sz, Sz)], hopefully allows many measurements eventually? # why use this over G2 blocks? 
         A["Sigma_iw"] = S.Sigma_iw
-        A["Sigma_tau"] = S.Sigma_tau
+        #A["Sigma_tau"] = S.Sigma_tau
         # A['F_tau'] = S.F_tau
         # A['nn_tau'] = S.nn_tau
         # A['nn'] = S.nn_static
