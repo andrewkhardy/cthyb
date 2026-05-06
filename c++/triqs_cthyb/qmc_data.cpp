505abfcc8b6abda9d453ef4af9dcf1920c485b7c
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-05-06 09:09:00 -0400

    cleaning

--- c++/triqs_cthyb/qmc_data.cpp
+++ c++/triqs_cthyb/qmc_data.cpp
@@ -1,4 +1,3 @@
 #include "qmc_data.hpp"
 #include <triqs/utility/legendre.hpp>
 
-// Just checking if qmc_data is header only. If so I'll write inline methods.
--- c++/triqs_cthyb/qmc_data_analytic.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cmath>
-#include <vector>
-#include <triqs/utility/legendre.hpp>
-
-// This logic goes to qmc_data.hpp directly
