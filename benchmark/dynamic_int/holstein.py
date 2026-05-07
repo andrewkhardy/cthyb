4b1b7fdd827372cacec67152157d8b450b8be4c8
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-05-07 15:04:15 -0400

    OK

--- benchmark/dynamic_int/holstein.py
+++ benchmark/dynamic_int/holstein.py
@@ -63,10 +63,10 @@ g0 << SemiCircular(2*hopping)
 ivn = np.array([x.imag for x in Q_iw["up", "up"].mesh.values()])
 zero_freq = np.where(np.abs(ivn) < 1e-10)
 mu = U/2 + np.real((Q_iw["up", "up"].data[zero_freq][0,0,0]+Q_iw["up", "down"].data[zero_freq][0,0,0]))/2.0
-print("OK")
-print(delta)
 
 g0 << SemiCircular(2*hopping)
+print("crap")
+
 S.Delta_tau << Fourier(g0)
 print("crap")
 
