314a012614ac56244046580636ff6ab095cddd60
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-05-07 15:12:16 -0400

    OK

--- benchmark/dynamic_int/spin_spin.py
+++ benchmark/dynamic_int/spin_spin.py
@@ -61,6 +61,7 @@ with h5.HDFArchive("ctint.ref.h5", 'r') as Af:
 # Hybridization Delta(tau)
 n_iw = len(g0.mesh)
 print(n_iw)
+print("WHAT")
 Delta = GfImFreq(indices=[0], beta=beta, n_points=n_iw)
 invg0 = GfImFreq(indices=[0], beta=beta, n_points=n_iw)
 Q_tau = GfImTime(indices=[0],  statistic='Boson', beta=beta, n_points=n_tau_bosonic)
