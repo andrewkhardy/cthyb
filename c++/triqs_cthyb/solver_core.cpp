ef1e5b2b0fbb1a6dae6675a3579f8ded3b007a23
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-05-11 18:02:53 -0400

    OK

--- c++/triqs_cthyb/solver_core.cpp
+++ c++/triqs_cthyb/solver_core.cpp
@@ -486,7 +486,7 @@ namespace triqs_cthyb {
                 }
                 if (!is_nonzero) continue;
 
-                auto D0_eval = [D0_bl, i1, i2](double tau) -> double { return real(D0_bl[closest_mesh_pt(tau)](i1, i2)) / 2.0; };
+                auto D0_eval = [D0_bl, i1, i2](double tau) -> double { return real(D0_bl[closest_mesh_pt(tau)](i1, i2)); };
                 
                 auto d_n = fit_legendre_coeffs(n_pt_tau, beta, D0_eval, N_leg);
                   nda::vector<double> k_n_vec = M_matrix * d_n;
@@ -540,7 +540,7 @@ namespace triqs_cthyb {
                 // Create lambda function to evaluate D0(tau) for this block pair
                 // Make a copy of the specific block for the lambda
                 auto D0_block    = inputs.D0t(bl1, bl2);
-                auto D0_function = [D0_block, i1, i2](double tau) -> double { return real(D0_block[closest_mesh_pt(tau)](i1, i2)) / 2.0; };
+                auto D0_function = [D0_block, i1, i2](double tau) -> double { return real(D0_block[closest_mesh_pt(tau)](i1, i2)); };
                 dyn_interactions.emplace_back(D0_function);
 
                 if (params.verbosity >= 2) {
