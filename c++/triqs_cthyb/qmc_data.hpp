d12cb793e25a8a8c8f0d656bae834fba84f7e855
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-05-19 14:28:41 -0400

    weird

--- c++/triqs_cthyb/qmc_data.hpp
+++ c++/triqs_cthyb/qmc_data.hpp
@@ -209,15 +209,15 @@ double compute_lang_firsov_ratio(std::vector<std::pair<time_pt, op_desc>> const&
     }
   };
 
-  // for (auto const& p_in : inserted) {
-  //   interact_bg(p_in, 1.0);
-  //   delta_W += 0.5 * eval_K(p_in.second, p_in.second, p_in.first, p_in.first);
-  // }
+  for (auto const& p_in : inserted) {
+    interact_bg(p_in, 1.0);
+    delta_W += 0.5 * eval_K(p_in.second, p_in.second, p_in.first, p_in.first);
+  }
 
-  // for (auto const& p_rm : removed) {
-  //   interact_bg(p_rm, -1.0);
-  //   delta_W -= 0.5 * eval_K(p_rm.second, p_rm.second, p_rm.first, p_rm.first);
-  // }
+  for (auto const& p_rm : removed) {
+    interact_bg(p_rm, -1.0);
+    delta_W -= 0.5 * eval_K(p_rm.second, p_rm.second, p_rm.first, p_rm.first);
+  }
 
   for (size_t i = 0; i < inserted.size(); ++i) {
     for (size_t j = i + 1; j < inserted.size(); ++j) {
