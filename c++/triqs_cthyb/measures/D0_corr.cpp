6ecdf577e32c5c1379210a7717a87ccae74aecfa
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-05-25 13:13:49 -0400

    normalization

--- c++/triqs_cthyb/measures/D0_corr.cpp
+++ c++/triqs_cthyb/measures/D0_corr.cpp
@@ -91,7 +91,7 @@ namespace triqs_cthyb {
         for (int n = 0; n < n_leg; ++n) {
           mc_weight_t sum = 0.0;
           for (int p = 0; p < n_leg; ++p) sum += M(p, n) * (alpha_n(a, b, p) / norm);
-          q_n(a, b, n) = sum / (2.0 * n + 1.0);
+          q_n(a, b, n) = sum / (beta * (2.0 * n + 1.0));
         }
       }
     }
