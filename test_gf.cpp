efc40ed5d4f1ae463b078e7a016b3c6af6540ac9
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-06-09 12:23:28 -0400

    here is the finished state

--- test_gf.cpp
+++
@@ -1,7 +0,0 @@
-#include <triqs/gfs.hpp>
-using namespace triqs::gfs;
-int main() {
-    auto b = make_block2_gf<legendre>({10.0, Boson, 100}, gf_struct_t{{"up", 1}, {"down", 1}});
-    std::cout << "Success!" << std::endl;
-    return 0;
-}
