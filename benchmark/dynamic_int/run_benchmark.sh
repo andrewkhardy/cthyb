fb5704e3c675e5bc9dce72f28e6df324109a4375
Andrew Hardy <ahardy@rusty1.cm.cluster>
2026-08-12 16:37:27 -0400

    so stressed

--- benchmark/dynamic_int/run_benchmark.sh
+++ benchmark/dynamic_int/run_benchmark.sh
@@ -14,6 +14,7 @@ MODULES="devenv9/clang-py3-mkl llvm/20"
 module purge
 module use /mnt/home/wentzell/opt/modules
 module load ${MODULES}
+#module load triqs/multiorbital
 module load triqs/unstable
 ##################### run your code here #####################
 #../CTHYB/benchmark/
@@ -22,6 +23,6 @@ module load triqs/unstable
 #source /mnt/home/ahardy/ccq-software-build/triqs/3_development/installation/share/triqs/triqsvars.sh
 
 #mpirun -n 120 python spin_spin_modified.py --J -1.0 --U 4.0 --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0 --n_cycles 1000000 --measure_O_tau 5 --dyn_n_l 10
-mpirun -n 96 python spin_spin.py --J -1.0 --U 4.0 --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0 --n_cycles 1000000 --measure_O_tau 15 --dyn_n_l 10
-#mpirun -n 120 python ctseg_spin_spin.py --J 1.0 --U 4.0  --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
+mpirun -n 96 python spin_spin.py --J 0.5 --U 4.0 --i1 1.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0 --n_cycles 1000000 --measure_O_tau 22 --dyn_n_l 22
+#mpirun -n 96 python ctseg_spin_spin.py --J 1.0 --U 4.0  --i1 0.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0
 #mpirun python multiorb_spin_spin.py --J 1.0 --U 4.0 --i1 1.0 --i2 1.0 --i3 1.0 --i4 1.0  --i5 1.0 --beta 10.0 --n_cycles 10000000 --measure_O_tau 10
